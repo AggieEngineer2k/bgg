7ae2baabd2f22bd6cb944bf4806637acf96bac2b
Justin L. Brown <AggieEngineer2k@hotmail.com>
2023-01-14 22:20:09 -0600

    refac exp & acc to xmlapi(1)

--- FetchNeededStuff.ps1
+++ FetchNeededStuff.ps1
@@ -1,5 +1,7 @@
-﻿$baseUrl = 'https://boardgamegeek.com/xmlapi2/'
-$requestThrottle = 3
+﻿$xmlapi1BaseUrl = 'https://boardgamegeek.com/xmlapi/'
+$xmlapi2BaseUrl = 'https://boardgamegeek.com/xmlapi2/'
+$requestThrottle = 2
+$username = 'AggieEngineer2k'
 
 enum Subtype {
     boardgame
@@ -9,9 +11,9 @@ enum Subtype {
 
 <#
 .SYNOPSIS
-   Fetch a user's collection that they own
+   Fetch a user's collection
 #>
-function Fetch-BoardGameGeekCollectionOwned {
+function Fetch-BoardGameGeekCollection {
     [CmdletBinding()]
     [OutputType([PSCustomObject])]
     Param
@@ -28,12 +30,16 @@ function Fetch-BoardGameGeekCollectionOwned {
 
         # Subtype to exclude
         [Parameter(Mandatory=$false)]
-        [Subtype]$excludesubtype
+        [Subtype]$excludesubtype,
+
+        # Filter on owned
+        [Parameter(Mandatory=$false)]
+        [bool]$own = $null
     )
 
     $response = $null
     try {
-        $url = "{0}collection?username={1}&subtype={2}&own=1" -f 
+        $url = "{0}collection?username={1}&subtype={2}" -f 
             $script:baseUrl, 
             [uri]::EscapeDataString($username),
             [uri]::EscapeDataString($subtype),
@@ -42,6 +48,11 @@ function Fetch-BoardGameGeekCollectionOwned {
             $url = "{0}&excludesubtype={1}" -f
                 $url,
                 [uri]::EscapeDataString($excludesubtype)
+        }
+        if($own -ne $null) {
+            $url = "{0}&own={1}" -f
+                $url,
+                $(if ($own -eq $true) {'1'} else {'0'})
         }
         $request = { Invoke-WebRequest -Method Get -Uri $url -UseBasicParsing }
         $response = Invoke-Command -ScriptBlock $request
@@ -52,8 +63,48 @@ function Fetch-BoardGameGeekCollectionOwned {
     } catch [System.Net.WebException] {      
         Write-Error $_.Exception.Message
     } catch {
-        Write-Warning $_.Exception.GetType().FullName -ForegroundColor Yellow
-        Write-Warning $_.Exception -ForegroundColor Red
+        Write-Warning $_.Exception.GetType().FullName
+        Write-Warning $_.Exception -ForegroundColor
+    }
+
+    # Internal throttle to slow requests
+    sleep -Seconds $script:requestThrottle
+
+    return [xml]$response.Content
+}
+
+<#
+.SYNOPSIS
+   Fetch a user's collection
+#>
+function Fetch-BoardGameGeekBoardgame {
+    [CmdletBinding()]
+    [OutputType([PSCustomObject])]
+    Param
+    (
+        # Username to fetch the collection for
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true,
+                   Position=0)]
+        [int]$gameid
+    )
+
+    $response = $null
+    try {
+        $url = "{0}boardgame/{1}" -f 
+            $script:xmlapi1baseUrl, 
+            $gameid
+        $request = { Invoke-WebRequest -Method Get -Uri $url -UseBasicParsing }
+        $response = Invoke-Command -ScriptBlock $request
+        while ($response.StatusCode -eq 202) {
+            sleep -Seconds $script:requestThrottle
+            $response = Invoke-Command -ScriptBlock $request
+        }
+    } catch [System.Net.WebException] {      
+        Write-Error $_.Exception.Message
+    } catch {
+        Write-Warning $_.Exception.GetType().FullName
+        Write-Warning $_.Exception
     }
 
     # Internal throttle to slow requests
@@ -90,15 +141,15 @@ function Fetch-BoardGameGeekThing {
             [uri]::EscapeDataString($type)
         $request = { Invoke-WebRequest -Method Get -Uri $url -UseBasicParsing }
         $response = Invoke-Command -ScriptBlock $request
-        while ($response.StatusCode -eq 202) {
+        while ($response.StatusCode -eq 202 -or $response.StatusCode -eq 429) {
             sleep -Seconds $script:requestThrottle
             $response = Invoke-Command -ScriptBlock $request
     }
     } catch [System.Net.WebException] {      
         Write-Error $_.Exception.Message
     } catch {
-        Write-Warning $_.Exception.GetType().FullName -ForegroundColor Yellow
-        Write-Warning $_.Exception -ForegroundColor Red
+        Write-Warning $_.Exception.GetType().FullName
+        Write-Warning $_.Exception
     }
 
     # Internal throttle to slow requests
@@ -107,80 +158,46 @@ function Fetch-BoardGameGeekThing {
     return [xml]$response.Content
 }
 
-<#
-.SYNOPSIS
-   Fetch a thing
-#>
-function Fetch-BoardGameGeekFamily {
-    [CmdletBinding()]
-    [OutputType([PSCustomObject])]
-    Param
-    (
-        # Username to fetch the collection for
-        [Parameter(Mandatory=$true,
-                   ValueFromPipeline=$true,
-                   Position=0)]
-        $id
-    )
-
-    $response = $null
-    try {
-        $url = "{0}family?id={1}" -f 
-            $script:baseUrl, 
-            [uri]::EscapeDataString($id)
-        $request = { Invoke-WebRequest -Method Get -Uri $url -UseBasicParsing }
-        $response = Invoke-Command -ScriptBlock $request
-        while ($response.StatusCode -eq 202) {
-            sleep -Seconds $script:requestThrottle
-            $response = Invoke-Command -ScriptBlock $request
-    }
-    } catch [System.Net.WebException] {      
-        Write-Error $_.Exception.Message
-    } catch {
-        Write-Warning $_.Exception.GetType().FullName -ForegroundColor Yellow
-        Write-Warning $_.Exception -ForegroundColor Red
-    }
-
-    # Internal throttle to slow requests
-    sleep -Seconds $script:requestThrottle
-
-    return [xml]$response.Content
-}
-
-#$collection = Fetch-BoardGameGeekCollectionOwned -username 'AggieEngineer2k' -subtype boardgame -excludesubtype boardgameexpansion
-#$collection.InnerXml | Out-File -FilePath "C:\Users\Justin L. Brown\Desktop\boardgame.xml"
-[xml]$collection = Get-Content -Path 'C:\Users\Justin L. Brown\Desktop\boardgame.xml'
+Write-Progress -Activity 'Fetching owned boardgames...'
+$collection = Fetch-BoardGameGeekCollection -username $username -own 1 -subtype boardgame -excludesubtype boardgameexpansion
+$collection.InnerXml | Out-File -FilePath "C:\Users\Justin L. Brown\Desktop\boardgame.xml"
+#[xml]$collection = Get-Content -Path 'C:\Users\Justin L. Brown\Desktop\boardgame.xml'
 $ownedBoardgameObjectIds = $collection.items.item.objectid
 
-#$collection = Fetch-BoardGameGeekCollectionOwned -username 'AggieEngineer2k' -subtype boardgameexpansion
-#$collection.InnerXml | Out-File -FilePath "C:\Users\Justin L. Brown\Desktop\boardgameexpansion.xml"
-[xml]$collection = Get-Content -Path 'C:\Users\Justin L. Brown\Desktop\boardgameexpansion.xml'
+Write-Progress -Activity 'Fetching owned expansions...'
+$collection = Fetch-BoardGameGeekCollection -username $username -own 1 -subtype boardgameexpansion
+$collection.InnerXml | Out-File -FilePath "C:\Users\Justin L. Brown\Desktop\boardgameexpansion.xml"
+#[xml]$collection = Get-Content -Path 'C:\Users\Justin L. Brown\Desktop\boardgameexpansion.xml'
 $ownedBoardgameExpansionObjectIds = $collection.items.item.objectid
 
-#$collection = Fetch-BoardGameGeekCollectionOwned -username 'AggieEngineer2k' -subtype boardgameaccessory
-#$collection.InnerXml | Out-File -FilePath "C:\Users\Justin L. Brown\Desktop\boardgameaccessory.xml"
+Write-Progress -Activity 'Fetching owned accessories...'
+$collection = Fetch-BoardGameGeekCollection -username $username -own 1 -subtype boardgameaccessory
+$collection.InnerXml | Out-File -FilePath "C:\Users\Justin L. Brown\Desktop\boardgameaccessory.xml"
 #[xml]$collection = Get-Content -Path 'C:\Users\Justin L. Brown\Desktop\boardgameaccessory.xml'
-#$ownedBoardgameAccessoryObjectIds = $collection.items.item.objectid
+$ownedBoardgameAccessoryObjectIds = $collection.items.item.objectid
 
-$expansionIds = [System.Collections.ArrayList]@()
+$knownExpansionIds = [System.Collections.ArrayList]@()
+$knownAccessoryIds = [System.Collections.ArrayList]@()
 
 $count = $ownedBoardgameObjectIds.Count
 for ($i = 0; $i -lt $count; $i++) {
-    $id = $ownedBoardgameObjectIds[$i]
-    $thing = Fetch-BoardGameGeekThing -id $id
-
     Write-Progress -Activity 'Iterating Boardgames' -Status "$($i + 1) of $count" -PercentComplete (($i/$count) * 100)
 
-    Select-Xml -Xml $thing -XPath '/items/item/link[@type="boardgameexpansion"]' | Select-Object -ExpandProperty Node | ForEach-Object { if ($ownedBoardgameExpansionObjectIds -notcontains $_.Id) { $expansionIds.Add($_.Id) | Out-Null } }
+    $id = $ownedBoardgameObjectIds[$i]
+    $boardgame = Fetch-BoardGameGeekBoardgame -gameid $id
+
+    Select-Xml -Xml $boardgame -XPath '/boardgames/boardgame/boardgameexpansion' | Select-Object -ExpandProperty Node | ForEach-Object { if ($ownedBoardgameExpansionObjectIds -notcontains $_.objectid) { $knownExpansionIds.Add($_.objectid) | Out-Null } }
+    Select-Xml -Xml $boardgame -XPath '/boardgames/boardgame/boardgameaccessory' | Select-Object -ExpandProperty Node | ForEach-Object { if ($ownedBoardgameAccessoryObjectIds -notcontains $_.objectid) { $knownAccessoryIds.Add($_.objectid) | Out-Null } }
 }
 
 $html = [System.Text.StringBuilder]::new()
 $html.Append('<html><body>') | Out-Null
 
+$html.Append('<h1>Expansions</h1>')
 $html.Append('<table>') | Out-Null
-$count = $expansionIds.Count
+$count = $knownExpansionIds.Count
 for ($i = 0; $i -lt $count; $i++) {
-    $id = $expansionIds[$i]
+    $id = $knownExpansionIds[$i]
     $thing = Fetch-BoardGameGeekThing -id $id -type boardgameexpansion
 
     $thumbnail = (Select-Xml -Xml $thing -XPath '/items/item/thumbnail').Node.InnerText
@@ -196,5 +213,25 @@ for ($i = 0; $i -lt $count; $i++) {
 }
 $html.Append('</table>') | Out-Null
 
+$html.Append('<h1>Accessories</h1>')
+$html.Append('<table>') | Out-Null
+$count = $knownAccessoryIds.Count
+for ($i = 0; $i -lt $count; $i++) {
+    $id = $knownAccessoryIds[$i]
+    $thing = Fetch-BoardGameGeekThing -id $id -type boardgameaccessory
+
+    $thumbnail = (Select-Xml -Xml $thing -XPath '/items/item/thumbnail').Node.InnerText
+    $name = (Select-Xml -Xml $thing -XPath '/items/item/name[@type="primary"]').Node.Value
+    $url = "https://boardgamegeek.com/boardgame/$id"
+
+    Write-Progress -Activity 'Iterating Accessories' -Status "$($i + 1) of $count" -PercentComplete (($i/$count) * 100)
+    
+    $html.Append('<tr>') | Out-Null
+    $html.Append("<td><img src='$thumbnail'/></td>") | Out-Null
+    $html.Append("<td><a href='$url'>$name</a></td>") | Out-Null
+    $html.Append('</tr>') | Out-Null
+}
+$html.Append('</table>') | Out-Null
+
 $html.Append('</body></html>') | Out-Null
-$html.ToString() | Out-File -FilePath 'C:\Users\Justin L. Brown\Desktop\expansions.html'
+$html.ToString() | Out-File -FilePath 'C:\Users\Justin L. Brown\Desktop\bgfomo.html'
